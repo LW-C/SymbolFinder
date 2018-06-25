97ccf2663e434b2847d428d123c5908ca760e16f
Lauren Wonicker-Cook <lwonickercook@ufl.edu>
2018-06-24 20:19:58 -0400

    Added the Symbol class

---
+++ src/Symbol.java
@@ -0,0 +1,26 @@
+public class Symbol
+{
+    private String symbolImage = "";
+    private String symbolName = "";
+
+    public void setSymbolName(String name)
+    {
+        symbolName = name;
+    }
+
+    public String getSymbolName()
+    {
+        return symbolName;
+    }
+
+    public void setSymbolImage(String image)
+    {
+        symbolImage = image;
+    }
+
+    public String getSymbolImage()
+    {
+        return symbolImage;
+    }
+
+}
---
+++ src/SymbolFinder.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SymbolFinder
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String userFigure = "";
+
+        System.out.println("Hello");
+        System.out.print("Please enter the name of the figure you wish to see: ");
+
+        return;
+    }
+}
